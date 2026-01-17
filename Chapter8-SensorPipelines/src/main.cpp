56dae35466f24da9538ba41371b79a9d65d3ac2b
Mark Wilson <mark@autumnalsoftware.com>
2026-01-17 10:46:35 -0500

    Removed unneeded #includes and some comments

--- Chapter8-SensorPipelines/src/main.cpp
+++ Chapter8-SensorPipelines/src/main.cpp
@@ -1,21 +1,12 @@
-#include <chrono>
-#include <iostream>
 #include <thread>
 
-#include "PositionConsumer.h"
-#include "MeasurementTypes.h"
-#include "MeasurementTypesIO.h"
-#include "PositionProducer.h"
 #include "SensorPipeline.h"
-
-// ----- PositionProducer -----
 
 using namespace weather;
 
 int main()
 {
     // "Capacity" is an initial sizing parameter for the SPSC queue.
-    // (We are not teaching strict boundedness in Option A.)
     SensorPipeline pipeline(/*capacity*/ 128);
 
     pipeline.start();
