99ff7a5355049ccdb4b8ab58594e11668757cd8e
Mark Wilson <mark@autumnalsoftware.com>
2026-01-22 11:56:43 -0500

    Added license header

--- Chapter7-ImprovedAnyMeasurement/include/AnyMeasurement.h
+++ Chapter7-ImprovedAnyMeasurement/include/AnyMeasurement.h
@@ -1,4 +1,8 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2025 Autumnal Software
+
 #pragma once
+
 #include "MeasurementTypes.h"
 
 #include <cassert>
--- Chapter7-ImprovedAnyMeasurement/include/MeasurementTypes.h
+++ Chapter7-ImprovedAnyMeasurement/include/MeasurementTypes.h
@@ -1,4 +1,8 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2025 Autumnal Software
+
 #pragma once
+
 #include <cstdint>
 
 namespace weather {
