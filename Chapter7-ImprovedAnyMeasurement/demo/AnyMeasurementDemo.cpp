ff78583871fa7433e52d429482f64807558daa88
Mark Wilson <mark@autumnalsoftware.com>
2026-01-22 12:01:13 -0500

    Updated demo and test for Empty

--- Chapter7-ImprovedAnyMeasurement/demo/AnyMeasurementDemo.cpp
+++ Chapter7-ImprovedAnyMeasurement/demo/AnyMeasurementDemo.cpp
@@ -1,11 +1,35 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2025 Autumnal Software
+
 #include <iostream>
+
 #include "AnyMeasurement.h"
 
 int main()
 {
     using namespace weather;
-    MeasurementHeaderV1 h{};
-    h.kind = MeasurementKind::Humidity; // overridden
-    AnyMeasurement m(h, Temperature{20.0});
-    std::cout << m.get<Temperature>().value << "\n";
+
+    // Normal measurement demo
+    {
+        MeasurementHeaderV1 h{};
+        h.kind = MeasurementKind::Humidity; // will be overridden by ctor
+
+        AnyMeasurement m(h, Temperature{20.0});
+
+        std::cout << "Temperature value = "
+                  << m.get<Temperature>().value << "\n";
+    }
+
+    // Empty measurement demo
+    {
+        MeasurementHeaderV1 h{};
+        h.kind = MeasurementKind::Empty;
+
+        AnyMeasurement e(h, Empty{});
+
+        std::cout << "Empty measurement kind = "
+                  << static_cast<int>(e.kind()) << "\n";
+    }
+
+    return 0;
 }
--- Chapter7-ImprovedAnyMeasurement/tests/AnyMeasurementTests.cpp
+++ Chapter7-ImprovedAnyMeasurement/tests/AnyMeasurementTests.cpp
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2025 Autumnal Software
+
 #include <cassert>
 #include <iostream>
 
@@ -6,12 +9,33 @@
 int main()
 {
     using namespace weather;
-    MeasurementHeaderV1 h{};
-    h.kind = MeasurementKind::WindSpeed;
-    AnyMeasurement a(h, WindSpeed{5.0});
-    assert(a.kind() == MeasurementKind::WindSpeed);
-    std::cout << "Passed" << std::endl;
-    assert(a.get<WindSpeed>().value == 5.0);
-    std::cout << "Passed" << std::endl;
+
+    // Normal measurement test
+    {
+        MeasurementHeaderV1 h{};
+        h.kind = MeasurementKind::WindSpeed; // will be overwritten by ctor
+
+        AnyMeasurement a(h, WindSpeed{5.0});
+
+        assert(a.kind() == MeasurementKind::WindSpeed);
+        assert(a.get<WindSpeed>().value == 5.0);
+
+        std::cout << "WindSpeed test passed\n";
+    }
+
+    // Empty measurement test
+    {
+        MeasurementHeaderV1 h{};
+        h.kind = MeasurementKind::Empty;
+
+        AnyMeasurement e(h, Empty{});
+
+        assert(e.kind() == MeasurementKind::Empty);
+        assert(e.try_get<WindSpeed>() == nullptr);
+        assert(e.try_get<Temperature>() == nullptr);
+
+        std::cout << "Empty test passed\n";
+    }
+
     return 0;
 }
