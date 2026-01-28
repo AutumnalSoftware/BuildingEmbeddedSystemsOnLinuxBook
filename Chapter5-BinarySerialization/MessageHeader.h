5210ac1e4dab4413449d0bb114d4c800d0804270
Mark Wilson <mark@autumnalsoftware.com>
2026-01-28 16:07:32 -0500

    Added magic number to MessageHeaderV1

--- Chapter5-BinarySerialization/MessageHeader.h
+++ Chapter5-BinarySerialization/MessageHeader.h
@@ -1,5 +1,14 @@
 // SPDX-License-Identifier: MIT
 // Copyright (c) 2025 Mark Wilson
+
+#include <cstdint>
+#include <cstddef>
+#include <cstring>
+
+#include "BdsCommon.h"
+#include "BinaryReadStream.h"
+#include "BinaryWriteStream.h"
+
 //
 //------------------------------------------------------------------------------
 // Fixed-size framing header with checksums
@@ -8,27 +17,31 @@
 // Reserved fields/bits must be zero in v1 and ignored by receivers.
 //------------------------------------------------------------------------------
 
+
 static constexpr Endianness HeaderWireEndianness = Endianness::Little;
+
+static constexpr std::uint32_t MessageMagicV1 = 0x31534442; // 'B''D''S''1' in LE
 
 struct MessageHeaderV1
 {
-    uint8_t  version = 1;
-    uint8_t  headerSize = 0;       // = sizeof(MessageHeaderV1)
-    uint8_t  payloadEndian = 0;    // 0=Little, 1=Big
-    uint8_t  headerFlags = 0;      // reserved bits (must be 0 in v1)
+    std::uint32_t magic = MessageMagicV1;
+    std::uint8_t  version = 1;
+    std::uint8_t  headerSize = 0;       // = sizeof(MessageHeaderV1)
+    std::uint8_t  payloadEndian = 0;    // 0=Little, 1=Big
+    std::uint8_t  headerFlags = 0;      // reserved bits (must be 0 in v1)
 
-    uint16_t serviceId = 0;
-    uint16_t messageType = 0;
+    std::uint16_t serviceId = 0;
+    std::uint16_t messageType = 0;
 
-    uint32_t payloadSize = 0;
+    std::uint32_t payloadSize = 0;
 
-    uint32_t flags = 0;            // reserved in v1
+    std::uint32_t flags = 0;            // reserved in v1
 
-    uint8_t  headerChecksum = 0;   // XOR of header bytes with this treated as 0
-    uint8_t  payloadChecksum = 0;  // XOR of payload bytes
-    uint16_t reserved = 0;         // must be 0 in v1
+    std::uint8_t  headerChecksum = 0;   // XOR of header bytes with this treated as 0
+    std::uint8_t  payloadChecksum = 0;  // XOR of payload bytes
+    std::uint16_t reserved = 0;         // must be 0 in v1
 };
-static_assert(sizeof(MessageHeaderV1) == 20, "MessageHeaderV1 must be fixed size");
+static_assert(sizeof(MessageHeaderV1) == 24, "MessageHeaderV1 must be fixed size");
 static_assert(std::is_standard_layout<MessageHeaderV1>::value,
               "MessageHeaderV1 must be standard-layout for offsetof()");
 
@@ -56,8 +69,9 @@ inline void finalizeChecksums(MessageHeaderV1& h, ImmutableByteView payload) noe
 
 inline BinaryWriteStream& writeHeaderV1(BinaryWriteStream& w, const MessageHeaderV1& h) noexcept
 {
-    w.writeUInt8(h.version)
-    .writeUInt8(h.headerSize)
+    w.writeUInt32(h.magic)
+        .writeUInt8(h.version)
+        .writeUInt8(h.headerSize)
         .writeUInt8(h.payloadEndian)
         .writeUInt8(h.headerFlags)
         .writeUInt16(h.serviceId)
@@ -73,8 +87,9 @@ inline BinaryWriteStream& writeHeaderV1(BinaryWriteStream& w, const MessageHeade
 
 inline BinaryReadStream& readHeaderV1(BinaryReadStream& r, MessageHeaderV1& h) noexcept
 {
-    r.readUInt8(h.version)
-    .readUInt8(h.headerSize)
+    r.readUInt32(h.magic)
+        .readUInt8(h.version)
+        .readUInt8(h.headerSize)
         .readUInt8(h.payloadEndian)
         .readUInt8(h.headerFlags)
         .readUInt16(h.serviceId)
@@ -90,11 +105,15 @@ inline BinaryReadStream& readHeaderV1(BinaryReadStream& r, MessageHeaderV1& h) n
 
 inline bool validateHeaderV1(const MessageHeaderV1& h) noexcept
 {
+    if ( h.magic != MessageMagicV1 )
+    {
+        return false;
+    }
     if (h.version != 1)
     {
         return false;
     }
-    if (h.headerSize != sizeof(MessageHeaderV1))
+    if (h.headerSize != static_cast<std::uint8_t>(sizeof(MessageHeaderV1)))
     {
         return false;
     }
